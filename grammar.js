bc54e9dff234a75cb327774e4d6b1e34f2f250d7
Masanori Ogino <167209+omasanori@users.noreply.github.com>
2024-07-20 10:00:26 +0900

    Implement units (WIP)

--- grammar.js
+++ grammar.js
@@ -1,11 +1,28 @@
 // Copyright 2024 Masanori Ogino
 // SPDX-License-Identifier: MIT
 
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
 module.exports = grammar({
   name: 'hare',
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => 'hello'
+    source_file: $ => repeat($._sub_unit),
+
+    _sub_unit: $ => seq(
+      $.import
+      // TODO: Implement declarations
+    ),
+
+    import: $ => choice(
+      seq('use', $.identifier, ';')
+      // TODO: Implement other variants
+    ),
+
+    // identifier: $ => choice($.name, seq($.name, '::', $.identifier))
+    identifier: $ => /[a-zA-Z_][0-9a-zA-Z_]*(?:::[a-zA-Z_][0-9a-zA-Z_]*)*/,
+
+    name: $ => /[a-zA-Z_][0-9a-zA-Z_]*/,
   }
 });
