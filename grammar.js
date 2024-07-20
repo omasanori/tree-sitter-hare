d0eb485351fb493f227a2cc57b4ca19df3beef27
Masanori Ogino <167209+omasanori@users.noreply.github.com>
2024-07-20 15:40:29 +0900

    Borrow utility funtions common in other grammars

--- grammar.js
+++ grammar.js
@@ -1,4 +1,5 @@
-// Copyright 2024 Masanori Ogino
+// Copyright 2023-2024 Amaan Qureshi
+// Copyright 2024      Masanori Ogino
 // SPDX-License-Identifier: MIT
 
 /// <reference types="tree-sitter-cli/dsl" />
@@ -43,3 +44,53 @@ module.exports = grammar({
     name: $ => /[a-zA-Z_][0-9a-zA-Z_]*/,
   }
 });
+
+/**
+ * Creates a rule to match optionally match one or more of the rules
+ * separated by a comma  and optionally ending with a comma
+ *
+ * @param {Rule} rule
+ *
+ * @return {SeqRule}
+ *
+ */
+function optionalCommaSep(rule) {
+  return seq(commaSep(rule), optional(','));
+}
+
+/**
+ * Creates a rule to match one or more of the rules separated by a comma
+ * and optionally ending with a comma
+ *
+ * @param {Rule} rule
+ *
+ * @return {SeqRule}
+ *
+ */
+function optionalCommaSep1(rule) {
+  return seq(commaSep1(rule), optional(','));
+}
+
+/**
+ * Creates a rule to optionally match one or more of the rules separated by a comma
+ *
+ * @param {Rule} rule
+ *
+ * @return {ChoiceRule}
+ *
+ */
+function commaSep(rule) {
+  return optional(commaSep1(rule));
+}
+
+/**
+ * Creates a rule to match one or more of the rules separated by a comma
+ *
+ * @param {Rule} rule
+ *
+ * @return {SeqRule}
+ *
+ */
+function commaSep1(rule) {
+  return seq(rule, repeat(seq(',', rule)));
+}
